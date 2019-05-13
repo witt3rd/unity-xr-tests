efe5a14140e78799e28c70d6d09fec6019991699
donald <witt3rd@witt3rd.com>
2019-05-13 10:04:52 -0800

    cube prefab+physics; spawn on button

--- Assets/ActionManager.cs
+++ Assets/ActionManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ActionManager : MonoBehaviour
 {
+    public GameObject thing;
+
     public void Button()
     {
+        Instantiate(thing, new Vector3(0.0F, 10.0F, -0.5F), Quaternion.identity);
         Debug.Log("Button pressed");
     }
 }
