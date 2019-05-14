13e01d0fe4fc41866926fed64b893488102fb907
donald <witt3rd@witt3rd.com>
2019-05-14 06:41:16 -0800

    warehouse

---
+++ Assets/ActionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour
+{
+    [SerializeField] public GameObject kindObject;
+
+        public void OnCreateKind()
+    {
+        Instantiate(kindObject, new Vector3(0, 10, .1F), Quaternion.identity);
+
+    }
+
+}
